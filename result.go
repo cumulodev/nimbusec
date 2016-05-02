713d2b4c284efb9f4a4dc0869ab69ab38414843a
Christof Horschitz <christof@nimbusec.com>
2016-05-02 09:22:33 +0200

    update nimbusec api sdk to match api and documentation

--- result.go
+++ result.go
@@ -2,12 +2,12 @@ package nimbusec
 
 // Result represents a finding of the nimbusec service that requires user action.
 type Result struct {
-	Id           int     `json:"id,omitempty`  // unique identification of a result
-	Status       string  `json:"status"`       // status of the result (pending, acknowledged, falsepositive, removed)
+	Id           int     `json:"id,omitempty"` // unique identification of a result
+	Status       int     `json:"status"`       // status of the result (1 = pending, 2 = acknowledged, 3 = falsepositive, 4 = removed)
 	Event        string  `json:"event"`        // event type of result (e.g added file)
 	Category     string  `json:"category"`     // category of result
 	Severity     int     `json:"severity"`     // severity level of result (1 = medium to 3 = severe)
-	Probability  float64 `json:"probability`   // probability the result is critical
+	Probability  float64 `json:"probability"`  // probability the result is critical
 	SafeToDelete bool    `json:"safeToDelete"` // flag indicating if the file can be safely deleted without loosing user data
 	CreateDate   int     `json:"createDate"`   // timestamp (in ms) of the first occurrence
 	LastDate     int     `json:"lastDate"`     // timestamp (in ms) of the last occurrence
